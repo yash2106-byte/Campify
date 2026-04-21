1874973c16777d4a7493d54dc90f6e9fff88d110
yash2106-byte <rajyash2510@gmail.com>
2026-04-21 19:02:59 +0530

    fixed index file

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ import * as login from "../Backend/src/controllers/login.controllers.js"
 
 const web = express()
 const port = 3000
-
+// we are useing this as a middleware because in our code we are working only on json
 web.use(express.json())
 web.get('/',(req,res)=>{
     return res.json("your request has reached")
