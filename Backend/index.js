f371ff139ef3283be96371e057a639493333912a
yash2106-byte <rajyash2510@gmail.com>
2026-04-21 18:36:56 +0530

    defined all the routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,17 @@
 import express from 'express'
-
-
-
+import * as signup from "../Backend/src/controllers/Signup.controllers.js"
+import * as login from "../Backend/src/controllers/login.controllers.js"
 
 const web = express()
 const port = 3000
 
-
+web.use(express.json())
 web.get('/',(req,res)=>{
     return res.json("your request has reached")
 })
+web.post('/signup',signup.PostSignup)
+web.post('/login',login.postLogin)
 
-
-
-web.use(express.json())
 web.listen(port,()=>{
     console.log(`the server is running ${port}`);
 });
